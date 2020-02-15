6ef2556340b253df8825c3d665f02944973036b1
Brian Sheridan <brian.sheridan@gmail.com>
2020-02-15 10:38:49 -0800

    Initial

---
+++ entrypoint.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+set -u
+
+function parseInputs(){
+	# Required inputs
+	if [ "${INPUT_CDK_SUBCOMMAND}" == "" ]; then
+		echo "Input cdk_subcommand cannot be empty"
+		exit 1
+	fi
+}
+
+function installAwsCdk(){
+	echo "Install aws-cdk ${INPUT_CDK_VERSION}"
+	if [ "${INPUT_CDK_VERSION}" == "latest" ]; then
+		npm install -g aws-cdk >/dev/null 2>&1
+		if [ "${?}" -ne 0 ]; then
+			echo "Failed to install aws-cdk ${INPUT_CDK_VERSION}"
+		else
+			echo "Successful install aws-cdk ${INPUT_CDK_VERSION}"
+		fi
+	else
+		npm install -g aws-cdk@${INPUT_CDK_VERSION} >/dev/null 2>&1
+		if [ "${?}" -ne 0 ]; then
+			echo "Failed to install aws-cdk ${INPUT_CDK_VERSION}"
+		else
+			echo "Successful install aws-cdk ${INPUT_CDK_VERSION}"
+		fi
+	fi
+}
+
+function installPipRequirements(){
+	if [ -e "requirements.txt" ]; then
+		echo "Install requirements.txt"
+		pip install -r requirements.txt >/dev/null 2>&1
+		if [ "${?}" -ne 0 ]; then
+			echo "Failed to install requirements.txt"
+		else
+			echo "Successful install requirements.txt"
+		fi
+	fi
+}
+
+function runCdk(){
+	echo "Run cdk ${INPUT_CDK_SUBCOMMAND} ${*} \"${INPUT_CDK_STACK}\""
+	output=$(cdk ${INPUT_CDK_SUBCOMMAND} ${*} "${INPUT_CDK_STACK}" 2>&1)
+	exitCode=${?}
+	echo ::set-output name=status_code::${exitCode}
+	echo "${output}"
+
+	commentStatus="Failed"
+	if [ "${exitCode}" == "0" -o "${exitCode}" == "1" ]; then
+		commentStatus="Success"
+	fi
+
+	if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${INPUT_ACTIONS_COMMENT}" == "true" ]; then
+		commentWrapper="#### \`cdk ${INPUT_CDK_SUBCOMMAND}\` ${commentStatus}
+<details><summary>Show Output</summary>
+
+\`\`\`
+${output}
+\`\`\`
+
+</details>
+
+*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`, Working Directory: \`${INPUT_WORKING_DIR}\`*"
+
+		payload=$(echo "${commentWrapper}" | jq -R --slurp '{body: .}')
+		commentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+
+		echo "${payload}" | curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${commentsURL}" > /dev/null
+	fi
+}
+
+function main(){
+	parseInputs
+	cd ${GITHUB_WORKSPACE}/${INPUT_WORKING_DIR}
+	installAwsCdk
+	installPipRequirements
+	runCdk
+}
+
+main
